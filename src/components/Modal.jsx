8d96993fc4739002e49c6a381f1ae130d341f5c0
Aditya <adityashrivastava2903@gmail.com>
2025-03-06 12:31:19 +0530

    Revert "Intigrate AdvancedMarkerElement with error handling"
    This reverts commit 838ed674d7972763be1ed923b228ae083d5b586f.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ function Modal({ data }) {
         ...new Set(data.data.map((location) => location.place)),
     ];
 
-    console.log(">>>>>>>>>>>>>", uniquePlaces);
+    // console.log(">>>>>>>>>>>>>", uniquePlaces);
 
     return (
         <>
