3567d3a29d6038d845ead63da223c87be615c34a
Manit-CD <manitcivildepartment.la@gmail.com>
2025-04-09 17:57:21 +0530

    Update api

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -12,7 +12,7 @@ function Content() {
         // const token = "007c7643f181104371e583667baccece44de71ab";
 
         const backendUrl =
-            "https://alert-backend-5mxl.onrender.com/auth/alerts/";
+            "https://alertapp.manit.ac.in/auth/alerts/";
 
         fetch(`/api/proxy?url=${encodeURIComponent(backendUrl)}`, {
             method: "GET",
